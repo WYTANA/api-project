c7d8c3aab8adfc0ab16c19def0913e37cb4080b0
torback <tlorback@gmail.com>
2022-12-30 21:58:00 -0700

    Update route handlers to read and write from fs

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
+const fs = require("fs")
 const express = require("express")
 
+// Use express
 const app = express()
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from the server!", dev: "Travis" })
+// Use middleware
+app.use(express.json())
+
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello from the server!", dev: "Travis" })
+// })
+
+// app.post("/", (req, res) => {
+//   res.send("Posting to the endpoint!")
+// })
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+// Route Handlers
+
+// GET
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  })
 })
 
-app.post("/", (req, res) => {
-  res.send("Posting to the endpoint!")
+// POST
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body)
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+
+  tours.push(newTour)
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        data: {
+          tour: newTour,
+        },
+      })
+    }
+  )
 })
 
+// Serve the data
 const port = 3000
 app.listen(port, () => {
   console.log(`App is running on Port: ${port}!`)
