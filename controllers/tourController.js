0a5a3ae1bf48dd6078bb558c4d8dab11b0093693
torback <tlorback@gmail.com>
2023-01-02 16:07:24 -0700

    Add param middleware and refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 )
 
+exports.checkId = (req, res, next, val) => {
+  console.log(`Tour ID is: ${val}`)
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    })
+  }
+  next()
+}
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime)
   res.status(200).json({
@@ -19,13 +30,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1
   const tour = tours.find((el) => el.id === id)
-
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
 
   res.status(200).json({
     status: "success",
@@ -56,13 +60,6 @@ exports.createTour = (req, res) => {
 }
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
-
   res.status(200).json({
     status: "success",
     data: {
@@ -72,15 +69,15 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    })
-  }
-
   res.status(204).json({
     status: "success",
     data: null,
   })
 }
+
+// if (!tour) {
+//     return res.status(404).json({
+//       status: "fail",
+//       message: "Invalid ID",
+//     })
+//   }
