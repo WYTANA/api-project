aa4f8e424b90469fc92224efcc22cd86d790eb8f
torback <tlorback@gmail.com>
2023-01-02 19:10:28 -0700

    Add env vars, dotenv pkg, and additional logic

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const userRouter = require("./routes/userRoutes")
 const app = express()
 
 // Use global middleware
-app.use(morgan("dev"))
-
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
+}
 app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   console.log("Hello from middleware!")
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require("dotenv")
+dotenv.config({ path: "./config.env" })
+
 const app = require("./app")
 
-const port = 3000
+const port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log(`App is running on Port: ${port}!`)
 })
