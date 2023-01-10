e5f3eceba89b7e954104b7c1f61fbe9b98285b84
torback <tlorback@gmail.com>
2023-01-09 18:28:18 -0700

    Build out schema and test with postman

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,7 +50,7 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: "Fail",
-      message: "Invalid data sent",
+      message: error,
     })
   }
 }
